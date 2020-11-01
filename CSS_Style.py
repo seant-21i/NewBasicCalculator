e9147cb25481ce663a5c0988f0fa63ff4a8618dc
seant <wave.i5.edm@gmail.com>
2020-10-31 18:44:29 -0600

    Correccion de boton "Acerca De"

---
+++ CSS_Style.py
@@ -0,0 +1,59 @@
+class Styles:
+    design = "QWidget{\n"\
+             "background-color: #263238;\n"\
+             "color: #CFD8DC;\n"\
+             "}\n"\
+             "QMenuBar::item:selected {\n"\
+             "background-color: #455A64;\n"\
+             "color: #CFD8DC;\n"\
+             "}\n"\
+             "QMenu::item:selected {\n"\
+             "background-color: #455A64;\n"\
+             "color: #CFD8DC;\n"\
+             "}\n"\
+             "QMenu::item:pressed {\n"\
+             "background-color: #37474F;\n"\
+             "color: #CFD8DC;\n"\
+             "}\n"\
+             "QPushButton:hover{\n"\
+             "background-color: #455A64;\n"\
+             "}\n"\
+             "QPushButton:pressed{\n"\
+             "background-color: #37474F;\n"\
+             "}\n"\
+             "QPushButton[accessibleName=\"number\"]{\n"\
+             "border-radius: 13px;\n"\
+             "border-color: #CC81F0;\n"\
+             "border-style: solid;\n"\
+             "border-width: 2px;\n"\
+             "}\n"\
+             "QPushButton[accessibleName=\"Op\"]{\n"\
+             "border-radius: 13px;\n"\
+             "border-color: #69F0AE;\n"\
+             "border-style: solid;\n"\
+             "border-width: 2px;\n"\
+             "}\n"\
+             "QPushButton[accessibleName=\"Fn\"]{\n"\
+             "border-radius: 13px;\n"\
+             "border-color: #F0C42B;\n"\
+             "border-style: solid;\n"\
+             "border-width: 2px;\n"\
+             "}\n"\
+             "QPushButton[accessibleName=\"Equals\"]{\n"\
+             "border-radius: 13px;\n"\
+             "border-color: #F0BB51;\n"\
+             "border-style: solid;\n"\
+             "border-width: 2px;\n"\
+             "}\n"\
+             "QPushButton[accessibleName=\"Exit\"]{\n"\
+             "border-radius: 13px;\n"\
+             "border-color: #C2185B;\n"\
+             "border-style: solid;\n"\
+             "border-width: 2px;\n"\
+             "}\n"\
+             "QLabel{\n"\
+             "border-radius: 7px;\n"\
+             "border-color: #D1C4E9;\n"\
+             "border-style: solid;\n"\
+             "border-width: 2px;\n"\
+             "}"
